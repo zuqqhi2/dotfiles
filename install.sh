0acb150bd6f1fe38ecb0e7d34cbe53175ea7c0a1
Hidetomo Suzuki <hidetomo@ip-10-245-47-13.us-west-2.compute.internal>
2013-04-11 09:06:42 +0000

    Revise

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TARGET="$HOME/.dot"
+if [ -d "$TARGET" ]; then
+  echo "Updating dotfiles repository ..."
+  cd $TARGET && git pull origin master
+else
+  git clone https://github.com/kashimoo/dot.git $TARGET
+fi
+
+BUNDLE="$TARGET/vim/bundle"
+if [ ! -d "$BUNDLE" ]; then
+  git clone https://github.com/Shougo/neobundle.vim.git $BUNDLE/neobundle.vim
+fi
+
+rm -rf $HOME/.vim
+rm -f $HOME/.vimrc
+rm -f $HOME/.screenrc
+ln -s $TARGET/vim $HOME/.vim
+ln -s $TARGET/vim/vimrc $HOME/.vimrc
+ln -s $TARGET/screenrc $HOME/.screenrc
+echo "Set dotfiles!"
